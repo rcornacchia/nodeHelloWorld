a586b46f3f845a22ddc62b61b071bec13143a565
Philippe-Guillaume Losembe <pglosembe@gmail.com>
2016-02-27 13:29:40 -0500

    Track tweets on box office top 10

--- TweetMap2/server.js
+++ TweetMap2/server.js
@@ -1,14 +1,45 @@
-// Setup web server and socket
-var twitter = require('ntwitter'),
+var twitter = require('twitter'),
     express = require('express'),
     elasticSearch = require('elasticsearch'),
-    app = express().createServer()
+    app = express();
 
-// Setup twitter stream api
-var twit = new twitter({
+// Setup twitter api
+var tClient = new twitter({
     consumer_key: 'fRT0vKluRXvF4biuL4f3u3aQH',
     consumer_secret: 'DI2Gkxma6IwYtzLC8qbIBY9ClfKxwliICAghkO3gXvL93OiAs7',
     access_token_key: '309561440-woeVVklGEKaTnx5jb2IzJ9ATuy1tAJpkimPRvI0v',
     access_token_secret: 'Kd9tLOi65pPcW4vqLC5X5UxbanzpHtMDi7jNAfS9uY5Rh'
-}),
-stream = null;
+});
+
+// // Set up elasticSearch client
+// var esClient = new elasticSearch.Client({
+//     host: 'search-tweetmapccbd-fbed2pdkzapesfu7nzk4fmtmsm.us-east-1.es.amazonaws.com',
+//     log: 'trace'
+// });
+
+// Connect to twitter stream websocket
+var keywords = [
+    'deadpool',
+    'kung fu panda',
+    'risen',
+    'the witch',
+    'how to be single',
+    'zoolander',
+    'star wars',
+    'the revenant',
+    'hail caesar!'
+];
+var params = {track:keywords.join(', ') };
+tClient.stream('statuses/filter', params, function(stream) {
+    stream.on('data', function(tweet) {
+        if (tweet.geo) {
+            console.log(tweet.id),
+            console.log(tweet.text),
+            console.log(tweet.coordinates),
+            console.log("====\n");
+        }
+        // client.create({
+        //     index:
+        // });
+    });
+});
