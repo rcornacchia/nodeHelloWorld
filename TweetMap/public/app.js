b138c2748bce77e806a9251ee9f161dd924076d5
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-03-03 16:30:25 -0500

    refactoring

--- TweetMap/public/app.js
+++ TweetMap/public/app.js
@@ -7,23 +7,6 @@ var map;
 var tweets = [];
 var gMarkers = [];
 
-function removeMarkers(gMarkers){
-    // for (var i = 0; i < gMarkers.length; i++ ) {
-    //     marker = gMarkers[i];
-    //
-    //     // marker = null;
-    //     gMarkers[i].setMap(null);
-    //     // gMarkers.splice(i, 1);
-    // }
-    for (var i = 0; i < gMarkers.length; i++) {
-        gMarkers[i].setMap(null);
-    }
-    gMarkers = [];
-}
-
-
-
-
 // function myCallback(data) {
 //     // console.log(data);
 //     var obj = JSON.parse(data);
@@ -61,63 +44,75 @@ function removeMarkers(gMarkers){
 //     xmlHttp.open("GET", theUrl, true); // true for asynchronous
 //     xmlHttp.send(null);
 // }
+function mapTweets(data) {
+    console.log(data);
+    tweets = [];
+    obj = data;
+    for(var i=0; i<obj.hits.hits.length; i++){
+        tweets.push([obj.hits.hits[i]._source.location, obj.hits.hits[i]._source.text]);
+    }
+    tweets.forEach(function(tweet) {
+        var position_options = {
+            lat: parseFloat(tweet[0].lat),
+            lng: parseFloat(tweet[0].lon)
+        };
+        var infowindow = new google.maps.InfoWindow({
+            content: tweet[1]
+        });
+        var marker = new google.maps.Marker({
+            position: position_options,
+            map: map
+        });
+        gMarkers.push(marker);
+
+        google.maps.event.addListener(marker, 'click', (function () {
+            infowindow.open(map, marker);
+        }));
+    });
+}
 
 function initMap() {
-    // httpGetAsync("test", myCallback);
     $(function(){
        $(".dropdown-menu li a").click(function(){
          $(".btn:first-child").text($(this).text());
          $(".btn:first-child").val($(this).text());
       });
     });
-
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 38, lng: -97},
         zoom: 5
     });
-
+    var marker = new google.maps.Marker({
+        // position: position_options,
+        // map: map
+    });
+    google.maps.event.addListener(marker, "click", function (event) {
+        console.log('test');
+        var latitude = event.latLng.lat();
+        var longitude = event.latLng.lng();
+        console.log( latitude + ', ' + longitude );
+    });
+    // fetch all tweets
+    var candidate = "All Candidates";
+    $.post("http://localhost:3000/getTweets",{candidate: candidate}, function(data){
+        mapTweets(data);
+    });
+    // delete marker function
     google.maps.Map.prototype.clearOverlays = function() {
       for (var i = 0; i < gMarkers.length; i++ ) {
         gMarkers[i].setMap(null);
       }
-      gMarkers.length = 0;
+      gMarkers = [];
     }
 }
 
 $(document).ready(function(){
-
     $(document.body).on('click', '.dropdown li a', function (e) {
-        console.log($(this).text());
         // delete existing markers
-        // removeMarkers(gMarkers);
         map.clearOverlays();
-        gMarkers = [];
         var candidate = $(this).text();
         $.post("http://localhost:3000/getTweets",{candidate: candidate}, function(data){
-            console.log(data);
-            tweets = [];
-            obj = data;
-            for(var i=0; i<obj.hits.hits.length; i++){
-                tweets.push([obj.hits.hits[i]._source.location, obj.hits.hits[i]._source.text]);
-            }
-            tweets.forEach(function(tweet) {
-                var position_options = {
-                    lat: parseFloat(tweet[0].lat),
-                    lng: parseFloat(tweet[0].lon)
-                };
-                var infowindow = new google.maps.InfoWindow({
-                    content: tweet[1]
-                });
-                var marker = new google.maps.Marker({
-                    position: position_options,
-                    map: map
-                });
-                gMarkers.push(marker);
-
-                google.maps.event.addListener(marker, 'click', (function () {
-                    infowindow.open(map, marker);
-                }));
-            });
+            mapTweets(data);
         });
     });
 });
